a8cbc486e70f42afe62abea744b8f321d35ae6d6
wushiyong <wushiyong@aixuexi.com>
2021-01-03 16:10:52 +0800

    init

---
+++ aliyun/loghub.py
@@ -0,0 +1,17 @@
+#-*- coding:utf-8 -*-
+import sys
+import os
+
+from aliyun.log import LogClient
+
+ALI_ACCESS_ID =  os.environ.get('ALI_ACCESS_ID', "")
+ALI_ACCESS_KEY =  os.environ.get('ALI_ACCESS_KEY', "")
+
+
+class LogHubHelper(LogClient):
+	"""docstring for DataHub
+	PS：功能太少，建议使用loghub
+	"""
+	def __init__(self, access_id = ALI_ACCESS_ID, access_key = ALI_ACCESS_KEY, endpoint = "cn-beijing.log.aliyuncs.com"):
+		super(LogHubHelper, self).__init__(endpoint, access_id, access_key)
+
